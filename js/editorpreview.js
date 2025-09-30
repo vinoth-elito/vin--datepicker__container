d87d354201bcc7c4a8d32ce4822e9700fcb07b84
Vinoth R <vinoth@elitoinnovations.com>
2025-09-30 15:40:17 +0530

    added cdn for preview.css

--- js/editorpreview.js
+++ js/editorpreview.js
@@ -1616,8 +1616,8 @@ async function loadAll() {
         stopFlag
     );
     const cssEditor = document.getElementById('css-editor');
-    // const cssUrl = `https://vinoth-elito.github.io/vin--datepicker__container/css/preview.css?v=${cacheBuster}`;
-    const cssUrl = `css/preview.css?v=${cacheBuster}`;
+    const cssUrl = `https://vinoth-elito.github.io/vin--datepicker__container/css/preview.css?v=${cacheBuster}`;
+    // const cssUrl = `css/preview.css?v=${cacheBuster}`;
     try {
         const res = await fetch(cssUrl, { cache: 'no-store' });
         cssEditor.value = await res.text();
