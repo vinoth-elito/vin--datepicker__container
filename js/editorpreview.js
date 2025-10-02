c7898ea221c8e95dbeb4f8291cd3cff168046d74
Vinoth R <vinoth@elitoinnovations.com>
2025-10-02 19:52:43 +0530

    Added layout for all sliders

--- js/editorpreview.js
+++ js/editorpreview.js
@@ -1787,6 +1787,10 @@ $('body').on('click', '.editor-sidebar button', function () {
     container.find('.editor-panel').removeClass('active');
     container.find('#' + dataactive).addClass('active');
 });
+window.addEventListener("beforeunload", function (e) {
+    e.preventDefault();
+    e.returnValue = "Changes could not update if page get refresh.";
+});
 function loadScripts() {
     const editor = document.getElementById('js-editor');
     const functionsToInclude = [
